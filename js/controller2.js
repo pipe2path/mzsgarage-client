a65d7c6797a7f40da44ce6907c56cbee0173a052
Kevin Menezes <kevinmenezes@yahoo.com>
2017-11-12 15:48:36 -0800

    adjusted spinner wait time

--- js/controller2.js
+++ js/controller2.js
@@ -55,7 +55,7 @@ app.controller('DashboardCtrl', ['$scope', '$timeout', '$http', '$q', '$filter',
             };
 
             // run the promises after waiting for 10 seconds after the first call (so the the image can be uploaded)
-            $q.when(needImage()).then($timeout(getImage, 30000));
+            $q.when(needImage()).then($timeout(getImage, 15000));
         }
 
         $scope.toggle = true;
